75de545f66899b5cf46a232bf163c2d632e2fef2
apmarshall <apmarshall@soren.tech>
2018-07-06 18:01:40 -0400

    mario pyramid implemented in c

---
+++ mario/mario.c
@@ -0,0 +1,32 @@
+# include <cs50.h>
+# include <stdio.h>
+
+int n;
+do
+{
+    n = get_int("How high should the pyramid be?:");
+}
+while (n < 0 || n > 23);
+
+for (i=0; i<n; i++ ) 
+{
+    // print spaces
+    for (j=0; j<n-i; j++ )
+    {
+        printf(" ");
+    }
+    // print hashes
+    for (j=0; j<i; j++)
+    {
+        printf("#");
+    }
+    // print gap
+    printf("  ");
+    // print hashes
+    for (j=0; j<i; j++)
+    {
+        printf("#");
+    }
+    // start new line
+    printf("\n");
+}
