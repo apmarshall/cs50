a10ae1a6949650ba90726909975bbbeb70a5ff4a
apmarshall <apmarshall@soren.tech>
2018-07-11 11:52:45 -0400

    Fix: nead a main function

--- mario/mario.c
+++ mario/mario.c
@@ -1,32 +1,35 @@
 # include <cs50.h>
 # include <stdio.h>
 
-int n;
-do
+int main(void)
 {
-    n = get_int("How high should the pyramid be?:");
-}
-while (n < 0 || n > 23);
+    int n;
+    do
+    {
+        n = get_int("How high should the pyramid be?:");
+    }
+    while (n < 0 || n > 23);
 
-for (i=0; i<n; i++ ) 
-{
-    // print spaces
-    for (j=0; j<n-i; j++ )
+    for (i=0; i<n; i++ ) 
     {
-        printf(" ");
+        // print spaces
+        for (j=0; j<n-i; j++ )
+        {
+            printf(" ");
+        }
+        // print hashes
+        for (j=0; j<i; j++)
+        {
+            printf("#");
+        }
+        // print gap
+        printf("  ");
+        // print hashes
+        for (j=0; j<i; j++)
+        {
+            printf("#");
+        }
+        // start new line
+        printf("\n");
     }
-    // print hashes
-    for (j=0; j<i; j++)
-    {
-        printf("#");
-    }
-    // print gap
-    printf("  ");
-    // print hashes
-    for (j=0; j<i; j++)
-    {
-        printf("#");
-    }
-    // start new line
-    printf("\n");
 }
