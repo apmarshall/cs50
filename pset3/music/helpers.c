d046f8564865edda5412331b7fd5d1dbaa936c77
apmarshall <apmarshall@soren.tech>
2018-09-03 20:38:12 -0400

    Imported files from cs50 cdn

---
+++ pset3/music/helpers.c
@@ -0,0 +1,28 @@
+// Helper functions for music
+
+#include <cs50.h>
+
+#include "helpers.h"
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+    // TODO
+}
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+}
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+    if (s == NULL) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
