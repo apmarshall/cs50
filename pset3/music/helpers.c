58ada5276189f2a16051757e417a1b2da6a3bdb9
apmarshall <apmarshall@soren.tech>
2018-09-03 20:55:33 -0400

    First passes at helper functions

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -7,13 +7,27 @@
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
+    string numerator = strtok(fraction, "/");
+    string denominator = strtok(NULL, "/");
+
+    if (denominator.atoi = 8) {
+        return numerator.atoi;
+    }
+    else if (denominator.atoi = 2) {
+        return numerator.atoi * 4;
+    }
+    else {
+        return numerator.atoi * 2;
+    }
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    // TODO: Calculate distance from given note to A4
+        // Step 1: Parse note to determine octave and nominal
+        // Step 2: Determine number of semitones between note and A4
+    // Calculate frequency as 2^n/12 * 440 (n = semitones calculated above)
 }
 
 // Determines whether a string represents a rest
